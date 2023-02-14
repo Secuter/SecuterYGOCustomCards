e4055577a1e74531e2c5707a85f92a7e724dda54
Secuter <109549119+Secuter@users.noreply.github.com>
2023-02-14 19:14:10 +0100

    Update c12345227.lua

--- script/secuter/c12345227.lua
+++ script/secuter/c12345227.lua
@@ -20,7 +20,7 @@ function s.filter(c)
 	return c:IsSetCard(0x20E) and c:IsDiscardable()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,c:GetHandler())
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,e:GetHandler())
 		and Duel.IsPlayerCanDraw(tp,1)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsPlayerCanSpecialSummonMonster(tp,s.TOKEN_ID,0x20E,TYPES_TOKEN,500,500,1,RACE_CYBERSE,ATTRIBUTE_LIGHT) end
