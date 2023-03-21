680c7c15a52fb674f5328320d246c2152f478e0a
Secuter <109549119+Secuter@users.noreply.github.com>
2023-03-21 19:00:04 +0100

    Update c12345409.lua

--- script/secuter/c12345409.lua
+++ script/secuter/c12345409.lua
@@ -12,7 +12,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
