ebfe610d9ab1a9b398effe954b8949ac20cc0033
Secuter <109549119+Secuter@users.noreply.github.com>
2023-03-24 10:57:04 +0100

    Update c12340044.lua

--- script/secuter/c12340044.lua
+++ script/secuter/c12340044.lua
@@ -5,7 +5,6 @@ function s.initial_effect(c)
 	c:EnableCounterPermit(0x306)
 	--activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
