52eef3f8916b28a663f946100c57d42fbd1c93c4
Secuter <dai.diego96@gmail.com>
2023-10-08 21:50:43 +0200

    New quick spell

---
+++ script/secuter/c12341030.lua
@@ -0,0 +1,103 @@
+--Forbidden Discus
+--Scripted by Secuter
+if not SECUTER_IMPORTED then Duel.LoadScript("secuter_utility.lua") end
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.con)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--to grave trigger
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetRange(LOCATION_ALL)
+	e2:SetCondition(s.regcon)
+	e2:SetOperation(s.regop)
+	e2:SetLabelObject(e1)
+	c:RegisterEffect(e2)
+	--banish trigger
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetRange(LOCATION_ALL)
+	e3:SetCondition(s.regcon)
+	e3:SetOperation(s.regop)
+	e3:SetLabelObject(e1)
+	c:RegisterEffect(e3)
+	--activate from hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e4:SetCondition(s.handcon)
+	c:RegisterEffect(e4)
+end
+--check trigger
+function s.cfilter(c,rc)
+	return c:IsReason(REASON_COST) and c==rc
+end
+function s.regcon(e,tp,eg,ep,ev,re,r,rp)
+	return re and eg and eg:IsExists(s.cfilter,1,nil,re:GetHandler())
+end
+function s.regop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(id,RESET_CHAIN,0,1)
+	e:GetLabelObject():SetLabelObject(re)
+end
+--activate from hand
+function s.handcon(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),LOCATION_ONFIELD,0)==0
+end
+--activate
+function s.con(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(id)>0 and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainDisablable(ev) and re==e:GetLabelObject()
+end
+function s.filter(c)
+	return c:IsMonster() and (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg:GetFirst(),1,1-tp,LOCATION_MZONE)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateEffect(ev)
+	local tc=eg:GetFirst()
+	if tc then
+		local c=e:GetHandler()
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+		e1:SetTarget(s.distg)
+		e1:SetLabel(tc:GetOriginalCodeRule())
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetCode(EVENT_CHAIN_SOLVING)
+		e2:SetCondition(s.discon)
+		e2:SetOperation(s.disop)
+		e2:SetLabel(tc:GetOriginalCodeRule())
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
+	end
+end
+function s.distg(e,c)
+	local code=e:GetLabel()
+	local code1,code2=c:GetOriginalCodeRule()
+	return code1==code or code2==code
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	local code=e:GetLabel()
+	local code1,code2=re:GetHandler():GetOriginalCodeRule()
+	return re:IsActiveType(TYPE_MONSTER) and (code1==code or code2==code)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	Duel.NegateEffect(ev)
+end
