3ce6bfc3ebd1c9c5eaea91afbfa7a9afe2342ab5
Secuter <dai.diego96@gmail.com>
2023-10-17 22:27:32 +0200

    Update c12342404.lua

--- script/secuter/c12342404.lua
+++ script/secuter/c12342404.lua
@@ -93,7 +93,7 @@ function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
