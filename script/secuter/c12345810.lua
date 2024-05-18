3ac7adb3899753ecdd40a2887c101d56649e2b24
Secuter <dai.diego96@gmail.com>
2024-05-18 18:02:17 +0200

    Bugfix

--- script/secuter/c12345810.lua
+++ script/secuter/c12345810.lua
@@ -6,10 +6,10 @@ function s.initial_effect(c)
 	--negate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
@@ -22,12 +22,14 @@ function s.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and c:IsLevelAbove(5)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    Debug.UniqueMessage(tp,Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil))
 	return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsEquipCard() and c:IsAbleToGraveAsCost()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    Debug.UniqueMessage(tp,Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_SZONE,0,1,nil))
 	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_SZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_SZONE,0,1,1,nil)
@@ -35,6 +37,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+    Debug.UniqueMessage(tp,Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil))
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,2,nil)
